7cf143461fdca81b7bf9bb863fa2f0fa9c3e6392
GrobKar <44326791+GrobKar@users.noreply.github.com>
2020-08-06 15:20:03 +0200

    added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -8,7 +8,7 @@ import java.util.Set;
 @Table(name = "vets")
 public class Vet extends Person {
 
-    @ManyToMany(fetch = FetchType.EAGER.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
         inverseJoinColumns = @JoinColumn(name = "specialiti_id"))
     private Set<Speciality> specialities = new HashSet<>();
